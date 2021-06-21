316ecb236c28e86ad8bee8e6ad792c6ed1a001b4
MasterAzazel <mike.schnyder@hotmail.com>
2021-06-21 02:25:58 +0200

    Implemented file export to XML and JSON.

--- Zbw.ITB1821H.OrderManager.UserInterface/MainWindowViewModel.cs
+++ Zbw.ITB1821H.OrderManager.UserInterface/MainWindowViewModel.cs
@@ -1,13 +1,20 @@
 ﻿using log4net;
 using MahApps.Metro.IconPacks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Win32;
 using Microsoft.Xaml.Behaviors.Core;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
+using System.Text.Json;
 using System.Windows.Input;
+using System.Xml;
+using System.Xml.Serialization;
 using ZbW.ITB1821H.OrderManager.Controls;
+using ZbW.ITB1821H.OrderManager.Model;
 using ZbW.ITB1821H.OrderManager.Model.Context;
 using ZbW.ITB1821H.OrderManager.UserInterface.Controls;
 using ZbW.ITB1821H.OrderManager.UserInterface.Util;
@@ -67,13 +74,30 @@ namespace ZbW.ITB1821H.OrderManager
         {
             try
             {
-                SaveFileDialog dialog = new SaveFileDialog() { DefaultExt = "json", FileName = "OrderManagerData", Filter = "JSON file|*.json|CSV file|*.csv|XML file|*.xml" };
+                SaveFileDialog dialog = new SaveFileDialog() { DefaultExt = "json", FileName = "OrderManagerData", Filter = "JSON file|*.json|XML file|*.xml" };
                 bool? result = dialog.ShowDialog();
                 if (result == true)
                 {
                     Stream fileStream = dialog.OpenFile();
                     StreamWriter sw = new StreamWriter(fileStream);
-                    sw.WriteLine("INSERT EXPORT DATA");
+                    // get json string
+                    string jsonString = System.Text.Json.JsonSerializer.Serialize(App.DbContext.Customers.ToList(), new JsonSerializerOptions() { WriteIndented = true });
+                    switch (dialog.SafeFileName.Split(".").Last())
+                    {
+                        case "json":
+                            // write json string to file
+                            sw.Write(jsonString);
+                            break;
+
+                        case "xml":
+                            // convert json string to xml string anf write to file
+                            XmlDocument doc = JsonConvert.DeserializeXmlNode("{'Customer':" + jsonString + "}", "OrderManagerData");
+                            //doc.InnerXml
+                            XmlWriter writer = XmlWriter.Create(fileStream, new XmlWriterSettings { Indent = true });
+                            doc.WriteTo(writer);
+                            writer.Flush();
+                            break;
+                    }
                     sw.Flush();
                     sw.Close();
                 }
