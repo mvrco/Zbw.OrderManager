2666c3068637cbb2996adbf69f612a1b7067a94f
mvrco <mgachii@gmail.com>
2021-08-24 17:41:28 +0200

    removed unnecessary using statements

--- ZbW.ITB1821H.OrderManager.Model/DbConfiguration/ArticleConfiguration.cs
+++ ZbW.ITB1821H.OrderManager.Model/DbConfiguration/ArticleConfiguration.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ZbW.ITB1821H.OrderManager.Model.Entities;
 
 namespace ZbW.ITB1821H.OrderManager.Model.DbConfiguration
 {
--- ZbW.ITB1821H.OrderManager.Model/DbConfiguration/OrderConfiguration.cs
+++ ZbW.ITB1821H.OrderManager.Model/DbConfiguration/OrderConfiguration.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ZbW.ITB1821H.OrderManager.Model.Entities;
 
 namespace ZbW.ITB1821H.OrderManager.Model.DbConfiguration
 {
